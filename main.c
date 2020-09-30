eeff681763b15b529978cdf7722b2fefcf6753f3
Marcos Vinicius <marcosporto1963@gmail.com>
2020-09-30 22:58:00 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+  int numero,x;
+
+  x=1;
+  //ENQUANTO
+
+  while(x<=100000000000000000){
+ 
+printf("%d\n", x);
+//x=x+1;
+//x+=1;
+x++;
+
+  }
+  return 0;
+}
